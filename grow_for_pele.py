31b93c590434c2578b0b45d8f7adc42c70459f80
Carles Perez <carlesperez94@gmail.com>
2018-01-10 18:31:20 +0100

    Visual code and changes to fix running errors (solved). Example ready to run

--- grow_for_pele.py
+++ grow_for_pele.py
@@ -10,18 +10,27 @@ import simulations_linker
 
 # Logging constants
 LOG_FILENAME = "output.out"
-LOG_FORMAT = "%(asctime)s:%(levelname)s:%(message)s"
-
+LOG_FORMAT = "%(asctime)s:%(name)s:%(levelname)s:%(message)s"
+STREAM_FORMAT = "%(asctime)s:%(message)s"
+TEMPLATE_MESSAGE = "We are going to transform the template _{}_ into _{}_ in _{}_ steps! Starting..."
+SELECTED_MESSAGE = "\n============ Files selected ============\nControl file: {}\nPDB file: {}\nResults folder name: {}\n"
+FINISH_SIM_MESSAGE = "SIMULATION FOR control_file_grw_{} COMPLETED!!! "
 # Logging definition block
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
-formatter = logging.Formatter(LOG_FORMAT)
+log_formatter = logging.Formatter(LOG_FORMAT)
+stream_formatter = logging.Formatter(STREAM_FORMAT)
 
 file_handler = logging.FileHandler(LOG_FILENAME)
-file_handler.setFormatter(formatter)
+file_handler.setFormatter(log_formatter)
+file_handler.setLevel(logging.NOTSET)
+
+stream_handler = logging.StreamHandler()
+stream_handler.setFormatter(stream_formatter)
 
 logger.addHandler(file_handler)
+logger.addHandler(stream_handler)
 
 
 def parse_arguments():
@@ -30,133 +39,122 @@ def parse_arguments():
 
         Output: list with all the user arguments
     """
-    parser = argparse.ArgumentParser(description="""From an input file, correspondent to the template of the initial structure of the ligand,
-    throught this function we will generate "x_fragments" intermediate templates until reach the final template,
-    modifing Vann Der Waals, bond lengths and deleting charges""")
+    parser = argparse.ArgumentParser(description="""From an input file, correspondent to the template of the initial 
+    structure of the ligand,this function will generate "x_fragments" intermediate templates until reach the final 
+    template,modifying Vann Der Waals, bond lengths and deleting charges.""")
 
     required_named = parser.add_argument_group('required named arguments')  # We have to do this in order
     # to print properly the required arguments when using the defined arguments method
 
     # Required arguments
-    required_named.add_argument("-i", "--initial",
-                                required=True,
+    required_named.add_argument("-i", "--initial", required=True,
                                 help="""input file correspondent to the 
                                 initial template for the ligand that you 
-                                want to grow""")
+                                want to grow.""")
     required_named.add_argument("-t", "--trans", required=True,
                                 help="""When an atom is transformed into another
                                 one we want to conserve the properties
                                 of the first one until being changed in the 
-                                last template. This has to be specified in 
-                                the program.""")
+                                last template. The atom name of the initial template
+                                that we want to transform into another of the final
+                                template has to be specified in 
+                                a text file separated by whitespaces.""")
     required_named.add_argument("-c", "--contrl", required=True,
-                                help='Initial control file')
+                                help='Initial control file.')
     required_named.add_argument("-p", "--pdb", required=True,
-                                help='Initial pdb file')
-    required_named.add_argument("-cr", "--criteria",
-                                required=True,
-                                help="""Name of the column used as criteria in order
-                                to select the template used as input for 
-                                succesive simulations.""")
+                                help="""Initial pdb file which already contain the ligand with 
+                                the fragment that we want to grow but with bond lengths correspondent 
+                                to the initial ligand (dummy-like).""")
 
     # In the future we will remove this argument
     required_named.add_argument("-f", "--final", required=True,
                                 help="""Input file correspondent to the
                                 final template for the ligand that you 
-                                want to reach""")
+                                want to get.""")
 
     # Optional arguments
-    parser.add_argument("-x", "--frag", type=int,
-                        default=10,
+    parser.add_argument("-x", "--frag", type=int, default=10,
                         help="""Number of intermediate templates that you want 
                              to generate""")
-
-    parser.add_argument("-r", "--resfold",
-                        default="growing_output",
+    parser.add_argument("-r", "--resfold", default="growing_output",
                         help="Name for results folder")
+    parser.add_argument("-cr", "--criteria", default="Binding Energy",
+                        help="""Name of the column used as criteria in order
+                             to select the template used as input for 
+                             successive simulations.""")
     args = parser.parse_args()
 
     return args.initial, args.final, args.frag, args.trans, args.contrl, args.pdb, args.resfold, args.criteria
 
 
-def main(template_initial, template_final,
-         n_files, transformation,
-         control_file, pdb,
-         results_f_name, criteria):
+def main(template_initial, template_final, n_files, transformation, control_file, pdb, results_f_name, criteria):
     """
-        Description:
+        Description: This function is the main core of the program. It creates N intermediate templates
+        and control files for PELE. Then, it perform N successive simulations automatically.
 
         Input:
 
+        "template_initial" --> Name of the input file correspondent to the initial template for the ligand that you
+                               want to grow.
+
+        "template_final" --> Name of the input file correspondent to the final template for the ligand that you want
+                             to get.
+
+        "n_files" --> Number of intermediate templates that you want to generate
+
+        "transformation" --> When an atom is transformed into another one we want to conserve the properties.
+                             The atom name of the initial template that we want to transform into another of
+                             the final template has to be specified in a text file separated by whitespaces.
+
+        "control_file" --> Initial control file used as template to generate intermediates control files.
+
+        "pdb" --> Initial pdb file which already contain the ligand with the fragment that we want to grow
+                  but with bond lengths correspondent to the initial ligand (dummy-like).
+
+        "results_f_name" --> Name for results folder
+
+        "criteria" --> Name of the column of the report file used as criteria in order to select the template
+                       used as input for successive simulations.
+
         Output:
 
-    """
+        First, intermediate control files and templates. Then, the results for each simulation and a pdb file for
+        each selected trajectory (the last selected trajectory is the final structure with the ligand grown).
+
+        """
 
     # Creating template files
-    logger.info(("We are going to transform the template _{}_ \
-                 into _{}_ in _{}_ steps! Starting...".format(
-        template_initial, template_final,
-        n_files)))
-
-    templates = template_fragmenter_2.fragmenter(template_initial,
-                                                 template_final,
-                                                 transformation,
-                                                 n_files)
+    logger.info((TEMPLATE_MESSAGE.format(template_initial, template_final, n_files)))
+    templates = template_fragmenter_2.fragmenter(template_initial, template_final, transformation, n_files)
     # Creating control files
-    logger.info(
-        """=================== Files selected ================\n
-        Control file: {} \n
-        PDB file: {} \n
-        Results folder name: {}\n""".format(
-            control_file,
-            pdb,
-            results_f_name)
-                )
-
-    control_files = simulations_linker.control_file_modifier(
-        control_file,
-        pdb,
-        results_f_name,
-        n_files
-    )
+    logger.info(SELECTED_MESSAGE.format(control_file, pdb, results_f_name))
+    control_files = simulations_linker.control_file_modifier(control_file, pdb, results_f_name, n_files)
 
     # Run Pele for each control file
 
     # for template, control_file in zip(templates, control_files):
     for n in range(0, n_files):
         # Run Pele
-        if not os.path.exists("{}_{}".format(results_f_name,
-                                             string.ascii_lowercase[n]
-                                             )):
-            os.mkdir("{}_{}".format(results_f_name,
-                                    string.ascii_lowercase[n])
-                     )
+        if not os.path.exists("{}_{}".format(results_f_name, string.ascii_lowercase[n])):
+            os.mkdir("{}_{}".format(results_f_name, string.ascii_lowercase[n]))
+            simulations_linker.simulation_runner("control_file_grw_{}".format(string.ascii_lowercase[n]))
+            logger.info("SIMULATION FOR control_file_grw_{} COMPLETED!!!!!!!".format(string.ascii_lowercase[n]))
+        else:
             simulations_linker.simulation_runner(
                 "control_file_grw_{}".format(string.ascii_lowercase[n])
             )
-        logger.info("SIMULATION FOR control_file_grw_{} COMPLETED!!!!!!!".format(
-            string.ascii_lowercase[n])
-                    )
+            logger.info(FINISH_SIM_MESSAGE.format(string.ascii_lowercase[n]))
         # Choose the best trajectory
-        template_selector.trajectory_selector("{}_{}".format(results_f_name,
-                                                             string.ascii_lowercase[n]
-                                                             ),
-                                              "{}_{}_tmp.pdb".format(pdb,
-                                                                     string.ascii_lowercase[n + 1]),
-                                              "{}".format(criteria)
-                                              )
-        if os.path.exists("{}_{}".format(pdb,
-                                         string.ascii_lowercase[n + 1])
-                          ):
-            logger.info("Step of the Trajectory selected in {}_{}.pdb".format(
-                pdb,
-                string.ascii_lowercase[n + 1])
-                        )
-        else:
-            logger.critical("We could not create {}_{}.pdb".format(pdb,
-                                                                   string.ascii_lowercase[n + 1])
-                            )
+        template_selector.trajectory_selector("{}_{}".format(results_f_name, string.ascii_lowercase[n]),
+                                              "{}_{}_tmp.pdb".format(pdb, string.ascii_lowercase[n + 1]),
+                                              "{}".format(criteria))
+        template_selector.change_ligandname("{}_{}_tmp.pdb".format(pdb, string.ascii_lowercase[n + 1]),
+                                            "{}_{}.pdb".format(pdb, string.ascii_lowercase[n + 1]))
+        if not os.path.isfile("{}_{}.pdb".format(pdb, string.ascii_lowercase[n + 1])):
+            logger.critical("We could not create {}_{}.pdb".format(pdb, string.ascii_lowercase[n + 1]))
             exit()
+        else:
+            logger.info("Step of the Trajectory selected in {}_{}.pdb".format(pdb, string.ascii_lowercase[n + 1]))
 
 
 if __name__ == '__main__':
