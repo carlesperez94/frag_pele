b6ff98983ab9e0057d12d1cb13d18d79ce020ab7
Carles Perez <carlesperez94@gmail.com>
2018-11-22 11:40:55 +0100

    folder_handler.py simplified

--- Helpers/folder_handler.py
+++ Helpers/folder_handler.py
@@ -7,36 +7,29 @@ import constants as c
 logger = logging.getLogger(__name__)
 
 
+def check_and_create_folder(path):
+    if not os.path.exists(path):
+        os.mkdir(path)
+
+
 def check_and_create_DataLocal():
     # Get current path
     curr_dir = os.path.abspath(os.path.curdir)
-    if not os.path.exists(os.path.join(curr_dir, "DataLocal")):
-        os.mkdir(os.path.join(curr_dir, "DataLocal"))
-    if not os.path.exists(os.path.join(curr_dir, "DataLocal/Templates")):
-        os.mkdir(os.path.join(curr_dir, "DataLocal/Templates"))
-    if not os.path.exists(os.path.join(curr_dir, "DataLocal/LigandRotamerLibs")):
-        os.mkdir(os.path.join(curr_dir, "DataLocal/LigandRotamerLibs"))
-    if not os.path.exists(os.path.join(curr_dir, "DataLocal/LigandRotamerLibs")):
-        os.mkdir(os.path.join(curr_dir, "DataLocal/LigandRotamerLibs"))
-    if not os.path.exists(os.path.join(curr_dir, "DataLocal/Templates/OPLS2005")):
-        os.mkdir(os.path.join(curr_dir, "DataLocal/Templates/OPLS2005"))
-    if not os.path.exists(os.path.join(curr_dir, "DataLocal/Templates/OPLS2005/HeteroAtoms")):
-        os.mkdir(os.path.join(curr_dir, "DataLocal/Templates/OPLS2005/HeteroAtoms"))
+    check_and_create_folder((os.path.join(curr_dir, "DataLocal")))
+    check_and_create_folder((os.path.join(curr_dir, "DataLocal/Templates")))
+    check_and_create_folder((os.path.join(curr_dir, "DataLocal/LigandRotamerLibs")))
+    check_and_create_folder((os.path.join(curr_dir, "DataLocal/Templates/OPLS2005")))
+    check_and_create_folder((os.path.join(curr_dir, "DataLocal/Templates/OPLS2005/HeteroAtoms")))
 
 
 def check_and_create_results_folder(results_folder):
-    if not os.path.exists(c.OUTPUT_FOLDER):
-        os.mkdir(c.OUTPUT_FOLDER)
-    if not os.path.exists(results_folder):
-        os.mkdir(results_folder)
+    check_and_create_folder(c.OUTPUT_FOLDER)
+    check_and_create_folder(results_folder)
 
 
 def check_and_create_pdb_clusters_folder(pdbout_folder, iteration):
     if iteration == 0:
-        if not os.path.exists(pdbout_folder):  # Create the folder if it does not exist
-            os.mkdir(pdbout_folder)
-        if not os.path.exists(os.path.join(pdbout_folder, "{}".format(iteration))):  # Do the same if the subfolder does not exist
-            os.mkdir((os.path.join(pdbout_folder, "{}".format(iteration))))  # Put the name of the iteration
+        check_and_create_folder(pdbout_folder)
+        check_and_create_folder(os.path.join(pdbout_folder, "{}".format(iteration)))
     else:
-        if not os.path.exists(os.path.join(pdbout_folder, "{}".format(iteration))):
-            os.mkdir(os.path.join(pdbout_folder, "{}".format(iteration)))
+        check_and_create_folder(os.path.join(pdbout_folder, "{}".format(iteration)))
