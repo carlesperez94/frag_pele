a3100c93966c81ecafe8a6ed228f8c180a08800b
Dani Soler <daniel.soler@e-campus.uab.cat>
2019-04-01 22:57:15 +0200

    Fix constants

--- FrAG/AdaptivePELE_repo/AdaptivePELE/constants/constants.py
+++ FrAG/AdaptivePELE_repo/AdaptivePELE/constants/constants.py
@@ -1,6 +1,7 @@
 from __future__ import absolute_import, division, print_function, unicode_literals
 import os
 import socket
+import FrAG.constants as cs
 machine = socket.getfqdn()
 
 print("MACHINE", machine)
