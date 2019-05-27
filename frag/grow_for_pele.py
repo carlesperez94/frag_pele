b90d1480e27b8cefe3fb2e1727087d6c7836cf7b
daniel <daniel.soler@e-campus.uab.cat>
2019-05-27 17:49:42 +0200

    Remove print for debug

--- frag/grow_for_pele.py
+++ frag/grow_for_pele.py
@@ -4,7 +4,6 @@ import time
 import glob
 import argparse
 import os
-print(os.path.join(os.path.dirname(os.path.abspath(__file__)), "AdaptivePELE_repo"))
 sys.path.append(os.path.join(os.path.dirname(os.path.abspath(__file__)), "AdaptivePELE_repo"))
 import logging
 from logging.config import fileConfig
