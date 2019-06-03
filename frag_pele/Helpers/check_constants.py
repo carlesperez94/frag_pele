5c96f8d61daa65a09cd037d1e72eba14d6125fdb
dani <daniel.soler@e-campus.uab.cat>
2019-06-03 07:54:08 +0200

    Fix bug on constants

--- frag_pele/Helpers/check_constants.py
+++ frag_pele/Helpers/check_constants.py
@@ -4,7 +4,7 @@ import frag_pele.constants as cs
 
 def check():
     if not os.path.exists(cs.SCHRODINGER_PY_PATH):
-        raise OSError("Schrodinger path {} not found. Change the harcoded path under frag_pele/constants.py".format(cs.cs.SCHRODINGER_PY_PATH))
+        raise OSError("Schrodinger path {} not found. Change the harcoded path under frag_pele/constants.py".format(cs.SCHRODINGER_PY_PATH))
     if not os.path.exists(cs.PATH_TO_PELE):
         raise OSError("Pele path {} not found. Change the harcoded path under frag_pele/constants.py".format(cs.PATH_TO_PELE))
     elif not os.path.exists(cs.PATH_TO_PELE_DATA):
