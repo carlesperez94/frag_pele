bcddab533ee9f5d7d1bc1d7e9e6ba6452433b340
Carles Perez <carlesperez94@gmail.com>
2019-10-01 14:19:40 +0200

    Explorative mode added. New test and arguments to modify the control file.

--- tests/test_all.py
+++ tests/test_all.py
@@ -6,8 +6,9 @@ import os
 
 def test_HT():
     subprocess.call("bash test_HT.sh".split())
-    assert os.path.exists("selected_result_aminoC1N1/epochequilibration_result_aminoC1N1_trajectory_1.10_BindingEnergy-52.54600000000001.pdb") \
-    or os.path.exists("selected_result_aminoC1N1/epochequilibration_result_aminoC1N1_trajectory_1.10_BindingEnergy-49.3207.pdb")
+    assert os.path.exists("selected_result_aminoC1N1/epochesampling_result_aminoC1N1_trajectory_1.10_BindingEnergy-52.54600000000001.pdb") \
+    or os.path.exists("selected_result_aminoC1N1/epochsampling_result_aminoC1N1_trajectory_1.10_BindingEnergy-49.3207.pdb")  \
+    or os.path.exists("selected_result_aminoC1N1/epochsampling_result_aminoC1N1_trajectory_1.11_BindingEnergy-48.7485.pdb")
 
 def test_mae():
     subprocess.call("bash test_mae.sh".split())
@@ -25,12 +26,14 @@ def test_rotamers():
 
 def test_criteria():
     subprocess.call("bash test_criteria.sh".split())
-    assert os.path.exists("selected_result_aminoC1N1/epochequilibration_result_aminoC1N1_trajectory_1.12_currentEnergy-13514.3.pdb")
+    assert os.path.exists("selected_result_aminoC1N1/epochsampling_result_aminoC1N1_trajectory_1.12_currentEnergy-13514.3.pdb") \
+    or os.path.exists("selected_result_aminoC1N1/epochsampling_result_aminoC1N1_trajectory_1.10_currentEnergy-13516.9.pdb")
 
 def test_chainame():
     subprocess.call("bash test_chain_core_frag.sh".split())
-    assert os.path.exists("selected_result_phenyl2C6C1/epochequilibration_result_phenyl2C6C1_trajectory_1.1_BindingEnergy-72.5229.pdb") \
-    or os.path.exists("selected_result_phenyl2C6C1/epochequilibration_result_phenyl2C6C1_trajectory_1.1_BindingEnergy-46.979.pdb")
+    assert os.path.exists("selected_result_phenyl2C6C1/epochesampling_result_phenyl2C6C1_trajectory_1.1_BindingEnergy-72.5229.pdb") \
+    or os.path.exists("selected_result_phenyl2C6C1/epochesampling_result_phenyl2C6C1_trajectory_1.1_BindingEnergy-46.979.pdb") \
+    or os.path.exists("selected_result_phenyl2C6C1/epochsampling_result_phenyl2C6C1_trajectory_1.1_BindingEnergy-45.3478.pdb")
 
 def test_temperature():
     subprocess.call("bash test_temperature.sh".split())
@@ -42,3 +45,7 @@ def test_temperature():
                     assert True
                 else:
                     assert False
+
+def test_EX():
+    subprocess.call("bash test_EX.sh".split())
+    assert os.path.exists("selected_result_aminoC1N1/epochsampling_result_aminoC1N1_trajectory_1.11_BindingEnergy-48.7485.pdb")
