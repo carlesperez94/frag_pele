4218a15163e32ff69f1dd0f08a2f61faf798e2d3
Carles Perez <carlesperez94@gmail.com>
2019-11-22 15:22:56 +0100

    Tests corrected

--- tests/test_all.py
+++ tests/test_all.py
@@ -6,17 +6,18 @@ import os
 def test_double():
     subprocess.call("bash test_double_bond.sh".split())
     assert os.path.exists("selected_result_carbonylC6-H7O1-C1/epochsampling_result_carbonylC6-H7O1-C1_trajectory_1.1_BindingEnergy-40.5463.pdb") \
-    or os.path.exists("selected_result_carbonylC6-H7O1-C1/epochsampling_result_carbonylC6-H7O1-C1_trajectory_1.1_BindingEnergy-40.1397.pdb")
+    or os.path.exists("selected_result_carbonylC6-H7O1-C1/epochsampling_result_carbonylC6-H7O1-C1_trajectory_1.1_BindingEnergy-40.1397.pdb") \
 
 def test_HT():
     subprocess.call("bash test_HT.sh".split())
     assert os.path.exists("selected_result_aminoC1N1/epochsampling_result_aminoC1N1_trajectory_1.10_BindingEnergy-45.4364.pdb") \
     or os.path.exists("selected_result_aminoC1N1/epochsampling_result_aminoC1N1_trajectory_1.10_BindingEnergy-49.3207.pdb")  \
-    or os.path.exists("selected_result_aminoC1N1/epochsampling_result_aminoC1N1_trajectory_1.11_BindingEnergy-48.7485.pdb")
+    or os.path.exists("selected_result_aminoC1N1/epochsampling_result_aminoC1N1_trajectory_1.11_BindingEnergy-48.7485.pdb") \
+    or os.path.exists("selected_result_aminoC1N1/epochsampling_result_aminoC1N1_trajectory_1.1_BindingEnergy-49.3262.pdb")
 
 def test_mae():
     subprocess.call("bash test_mae.sh".split())
-    assert os.path.exists("selected_result_aminoC1N1/trajectory_1.10.mae")
+    assert os.path.exists("selected_result_aminoC1N1/trajectory_1.8.mae")
 
 def test_rotamers():
     subprocess.call("bash test_rotamers.sh".split())
@@ -31,13 +32,15 @@ def test_rotamers():
 def test_criteria():
     subprocess.call("bash test_criteria.sh".split())
     assert os.path.exists("selected_result_aminoC1N1/epochsampling_result_aminoC1N1_trajectory_1.10_currentEnergy-13507.9.pdb") \
-    or os.path.exists("selected_result_aminoC1N1/epochsampling_result_aminoC1N1_trajectory_1.10_currentEnergy-13516.9.pdb")
+    or os.path.exists("selected_result_aminoC1N1/epochsampling_result_aminoC1N1_trajectory_1.10_currentEnergy-13516.9.pdb") \
+    or os.path.exists("selected_result_aminoC1N1/epochsampling_result_aminoC1N1_trajectory_1.1_currentEnergy-13500.1.pdb")
 
 def test_chainame():
     subprocess.call("bash test_chain_core_frag.sh".split())
     assert os.path.exists("selected_result_phenyl2C6C1/epochsampling_result_phenyl2C6C1_trajectory_1.1_BindingEnergy-51.5974.pdb") \
     or os.path.exists("selected_result_phenyl2C6C1/epochesampling_result_phenyl2C6C1_trajectory_1.1_BindingEnergy-46.979.pdb") \
-    or os.path.exists("selected_result_phenyl2C6C1/epochsampling_result_phenyl2C6C1_trajectory_1.1_BindingEnergy-45.3478.pdb")
+    or os.path.exists("selected_result_phenyl2C6C1/epochsampling_result_phenyl2C6C1_trajectory_1.1_BindingEnergy-45.3478.pdb") \
+    or os.path.exists("selected_result_phenyl2C6C1/epochsampling_result_phenyl2C6C1_trajectory_1.1_BindingEnergy-45.9295.pdb")
 
 def test_temperature():
     subprocess.call("bash test_temperature.sh".split())
