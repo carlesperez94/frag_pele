8dadebcf71511a4a677d640f2e7d6909c6e1e714
Alberto <Alberto.soutullo@hotmail.com>
2020-04-08 17:07:18 +0200

    Fixed Typo in pele parameter paths

--- frag_pele/frag/PeleParameters/tests/test_pele_parameter_paths.py
+++ frag_pele/frag/PeleParameters/tests/test_pele_parameter_paths.py
@@ -7,14 +7,14 @@ import unittest
 from frag_pele.frag.PeleParameters.pele_parameter_paths import PeleParameterPaths
 
 
-class TestPathHandler(unittest.TestCase):
+class TestPeleParameterPaths(unittest.TestCase):
 
     @classmethod
     def setUpClass(cls):
         cls.pele_parameters_paths = PeleParameterPaths("test_pele_dir", "test_pele_license", "test_data",
                                                        "test_documents")
 
-    def test_path_handler(self):
+    def test_pele_parameter_paths(self):
         self.assertEqual(self.pele_parameters_paths.pele_dir, "test_pele_dir")
         self.assertEqual(self.pele_parameters_paths.pele_license, "test_pele_license")
         self.assertEqual(self.pele_parameters_paths.data, "test_data")
