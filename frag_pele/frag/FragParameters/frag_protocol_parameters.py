8d6c374bee4a782411e5e52d6a5b5828491427ac
Alberto <Alberto.soutullo@hotmail.com>
2020-04-24 13:33:44 +0200

    added frag protocol parameters

--- frag_pele/frag/FragParameters/frag_protocol_parameters.py
+++ frag_pele/frag/FragParameters/frag_protocol_parameters.py
@@ -3,3 +3,24 @@
 # Third-Party Imports
 
 # Project Imports
+
+
+class FragProtocolParameters:
+
+    def __init__(self, only_prepare: bool, only_grow: bool):
+        """
+        :param only_prepare = todo description
+        :param only_grow = todo description
+        """
+        # Protocol
+        self._only_prepare = only_prepare
+        self._only_grow = only_grow
+
+    # Properties
+    @property
+    def only_prepare(self):
+        return self._only_prepare
+
+    @property
+    def only_grow(self):
+        return self._only_grow
