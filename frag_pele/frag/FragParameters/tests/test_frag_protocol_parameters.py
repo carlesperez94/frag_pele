36a1b75e28dcb5a4c543fd488eb4a7f6210ffad1
Alberto <Alberto.soutullo@hotmail.com>
2020-04-24 15:31:03 +0200

    tested frag protocol parameters

--- frag_pele/frag/FragParameters/tests/test_frag_protocol_parameters.py
+++ frag_pele/frag/FragParameters/tests/test_frag_protocol_parameters.py
@@ -1,5 +1,18 @@
 # Python Imports
+import unittest
 
 # Third-Party Imports
 
 # Project Imports
+from frag_pele.frag.FragParameters.frag_protocol_parameters import FragProtocolParameters
+
+
+class TestFragProtocolParameters(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        cls.protocol_parameters = FragProtocolParameters(True, True)
+
+    def test_protocol_parameters(self):
+        self.assertEqual(self.protocol_parameters.only_grow, True)
+        self.assertEqual(self.protocol_parameters.only_prepare, True)
