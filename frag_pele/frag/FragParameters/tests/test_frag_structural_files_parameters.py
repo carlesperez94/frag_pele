bc42056698b9e61fde0ae8dfd9861d3a5ec328d6
Alberto <Alberto.soutullo@hotmail.com>
2020-04-24 15:32:05 +0200

    tested frag structural files parameters

--- frag_pele/frag/FragParameters/tests/test_frag_structural_files_parameters.py
+++ frag_pele/frag/FragParameters/tests/test_frag_structural_files_parameters.py
@@ -1,5 +1,18 @@
 # Python Imports
+import unittest
 
 # Third-Party Imports
 
 # Project Imports
+from frag_pele.frag.FragParameters.frag_structural_files_parameters import FragStructuralFilesParameters
+
+
+class TestFragStructuralFilesParameters(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        cls.structural_files = FragStructuralFilesParameters("test_complex_pdb", "test_fragment_pdb")
+
+    def test_structural_files(self):
+        self.assertEqual(self.structural_files.fragment_pdb, "test_fragment_pdb")
+        self.assertEqual(self.structural_files.complex_pdb, "test_complex_pdb")
