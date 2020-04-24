1e24e2496797fccdc00f8fcadebaf42f4adeb5c3
Alberto <Alberto.soutullo@hotmail.com>
2020-04-24 13:33:06 +0200

    Added frag identification parameters

--- frag_pele/frag/FragParameters/frag_identification_parameters.py
+++ frag_pele/frag/FragParameters/frag_identification_parameters.py
@@ -3,3 +3,17 @@
 # Third-Party Imports
 
 # Project Imports
+
+
+class FragIdentificationParameters:
+
+    def __init__(self, ligand_id: str):
+        """
+        :param ligand_id: Name to identify the new ligand in certain folder.
+        """
+        # Identification Params
+        self._ligand_id = ligand_id
+
+    @property
+    def ligand_id(self):
+        return self._ligand_id
