5d221e77cb78128c2a74be3aafcc38b90d490ca0
Alberto <Alberto.soutullo@hotmail.com>
2020-04-24 15:32:19 +0200

    tested frag parameters

--- frag_pele/frag/FragParameters/tests/test_frag_parameters.py
+++ frag_pele/frag/FragParameters/tests/test_frag_parameters.py
@@ -1,5 +1,26 @@
 # Python Imports
+import unittest
+from unittest.mock import Mock
 
 # Third-Party Imports
 
 # Project Imports
+
+from frag_pele.frag.FragParameters.frag_parameters import FragParameters
+
+
+class TestFragParameters(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        mock = Mock()
+
+        cls.frag_parameters = FragParameters(mock, mock, mock, mock, mock, mock)
+
+    def test_frag_parameters(self):
+        self.assertTrue(hasattr(self.frag_parameters, "structural_files"), True)
+        self.assertTrue(hasattr(self.frag_parameters, "structural_configuration"), True)
+        self.assertTrue(hasattr(self.frag_parameters, "identification_parameters"), True)
+        self.assertTrue(hasattr(self.frag_parameters, "configuration_parameters"), True)
+        self.assertTrue(hasattr(self.frag_parameters, "protocol_parameters"), True)
+        self.assertTrue(hasattr(self.frag_parameters, "running_modes"), True)
