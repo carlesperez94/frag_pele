0042aa35e95e633eca4789e0ada7789e50850e26
Alberto <Alberto.soutullo@hotmail.com>
2020-04-24 15:31:47 +0200

    tested frag structural configuration parameters

--- frag_pele/frag/FragParameters/tests/test_frag_structural_configuration_parameters.py
+++ frag_pele/frag/FragParameters/tests/test_frag_structural_configuration_parameters.py
@@ -1,5 +1,25 @@
 # Python Imports
+import unittest
 
 # Third-Party Imports
 
 # Project Imports
+from frag_pele.frag.FragParameters.frag_structural_configuration_parameters import FragStructuralConfigurationParameters
+
+
+class TestFragStructuralConfigurationParameters(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        cls.structural_information = FragStructuralConfigurationParameters("test_core_atom", "test_fragment_atom",
+                                                                           "test_h_core", "test_h_frag", "test_c_chain",
+                                                                           "test_f_chain", 1.0)
+
+    def test_structural_configuration(self):
+        self.assertEqual(self.structural_information.core_atom, "test_core_atom")
+        self.assertEqual(self.structural_information.fragment_atom, "test_fragment_atom")
+        self.assertEqual(self.structural_information.h_core, "test_h_core")
+        self.assertEqual(self.structural_information.h_frag, "test_h_frag")
+        self.assertEqual(self.structural_information.c_chain, "test_c_chain")
+        self.assertEqual(self.structural_information.f_chain, "test_f_chain")
+        self.assertEqual(self.structural_information.threshold_clash, 1.0)
