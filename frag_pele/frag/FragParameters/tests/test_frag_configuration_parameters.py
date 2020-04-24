fff5425b9280c42013925ee8bcc615737037c4f7
Alberto <Alberto.soutullo@hotmail.com>
2020-04-24 15:30:18 +0200

    tested frag configuration parameters

--- frag_pele/frag/FragParameters/tests/test_frag_configuration_parameters.py
+++ frag_pele/frag/FragParameters/tests/test_frag_configuration_parameters.py
@@ -1,5 +1,19 @@
 # Python Imports
+import unittest
 
 # Third-Party Imports
 
 # Project Imports
+from frag_pele.frag.FragParameters.frag_configuration_parameters import FragConfigurationParameters
+
+
+class TestFragConfigurationParameters(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        cls.configuration_parameters = FragConfigurationParameters(1, "test_criteria", "test_sampling_control")
+
+    def test_configuration_parameters(self):
+        self.assertEqual(self.configuration_parameters.growing_steps, 1)
+        self.assertEqual(self.configuration_parameters.criteria, "test_criteria")
+        self.assertEqual(self.configuration_parameters.sampling_control, "test_sampling_control")
