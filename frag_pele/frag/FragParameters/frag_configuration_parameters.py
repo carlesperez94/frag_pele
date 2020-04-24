07d6bc223f383c8a72d819c7635a07a68dc0c93e
Alberto <Alberto.soutullo@hotmail.com>
2020-04-24 13:32:47 +0200

    Added frag configuration parameters

--- frag_pele/frag/FragParameters/frag_configuration_parameters.py
+++ frag_pele/frag/FragParameters/frag_configuration_parameters.py
@@ -3,3 +3,31 @@
 # Third-Party Imports
 
 # Project Imports
+
+
+class FragConfigurationParameters:
+
+    def __init__(self, growing_steps: int, criteria: str, sampling_control: str):
+        """
+        :param growing_steps: Number of Growing Steps (GS).
+        :param criteria: Name of the column of the report file to select the structures that will spawn in the
+        next GS. Additionally, this parameter will be the selection criteria to extract the best
+        structure after completing the growing.
+        :param sampling_control: templatized control file to be used in the sampling simulation.
+        """
+        self._growing_steps = growing_steps
+        self._criteria = criteria
+        self._sampling_control = sampling_control
+
+    # Properties
+    @property
+    def growing_steps(self):
+        return self._growing_steps
+
+    @property
+    def criteria(self):
+        return self._criteria
+
+    @property
+    def sampling_control(self):
+        return self._sampling_control
