60603384648fe39898d9084f456c1994973b401c
Alberto <Alberto.soutullo@hotmail.com>
2020-04-24 15:30:44 +0200

    tested frag configuration parameters

--- frag_pele/frag/FragParameters/tests/test_frag_identification_parameters.py
+++ frag_pele/frag/FragParameters/tests/test_frag_identification_parameters.py
@@ -1,5 +1,17 @@
 # Python Imports
+import unittest
 
 # Third-Party Imports
 
 # Project Imports
+from frag_pele.frag.FragParameters.frag_identification_parameters import FragIdentificationParameters
+
+
+class TestFragIdentificationParameters(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        cls.identification_parameters = FragIdentificationParameters("test_ligand_id")
+
+    def test_identification_parameters(self):
+        self.assertEqual(self.identification_parameters.ligand_id, "test_ligand_id")
