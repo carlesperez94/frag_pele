d14cabdd576a1b0542838edff7b11bd1c44c1785
Alberto <Alberto.soutullo@hotmail.com>
2020-04-24 15:31:24 +0200

    tested frag running modes parameters

--- frag_pele/frag/FragParameters/tests/test_frag_running_modes_parameters.py
+++ frag_pele/frag/FragParameters/tests/test_frag_running_modes_parameters.py
@@ -1,5 +1,18 @@
 # Python Imports
+import unittest
 
 # Third-Party Imports
 
 # Project Imports
+from frag_pele.frag.FragParameters.frag_running_modes_parameters import FragRunningModesParameters
+
+
+class TestFragRunningModesParameters(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        cls.running_modes = FragRunningModesParameters(True, True)
+
+    def test_running_modes(self):
+        self.assertEqual(self.running_modes.no_check, True)
+        self.assertEqual(self.running_modes.restart, True)
