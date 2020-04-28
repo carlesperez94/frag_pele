736dd91865faaeee347fc372de5db8325be4c9f9
Alberto <Alberto.soutullo@hotmail.com>
2020-04-28 19:47:32 +0200

    Refactor main (wip) to use group arguments. Frag runner on wip aswell

--- frag_pele/main.py
+++ frag_pele/main.py
@@ -13,6 +13,13 @@ from frag_pele import serie_handler
 import frag_pele.constants as c
 from frag_pele.Helpers.argument_parser import parse_arguments
 from frag_pele.frag.ClusterParameters.cluster_parameters import ClusterParameters
+from frag_pele.frag.FragParameters.frag_configuration_parameters import FragConfigurationParameters
+from frag_pele.frag.FragParameters.frag_identification_parameters import FragIdentificationParameters
+from frag_pele.frag.FragParameters.frag_parameters import FragParameters
+from frag_pele.frag.FragParameters.frag_protocol_parameters import FragProtocolParameters
+from frag_pele.frag.FragParameters.frag_running_modes_parameters import FragRunningModesParameters
+from frag_pele.frag.FragParameters.frag_structural_configuration_parameters import FragStructuralConfigurationParameters
+from frag_pele.frag.FragParameters.frag_structural_files_parameters import FragStructuralFilesParameters
 from frag_pele.frag.PeleParameters.pele_parameter_archives import PeleParameterArchives
 from frag_pele.frag.PeleParameters.pele_parameter_paths import PeleParameterPaths
 from frag_pele.frag.PeleParameters.pele_parameter_sim_values import PeleParameterSimulationValues
@@ -35,23 +42,95 @@ logger = logging.getLogger(__name__)
 curr_dir = os.path.abspath(os.path.curdir)
 
 
+def create_pele_parameter_object(pele_arguments_namespace):
+
+    pele_params_paths = PeleParameterPaths(pele_arguments_namespace.pele_dir, pele_arguments_namespace.license,
+                                           pele_arguments_namespace.data, pele_arguments_namespace.documents)
+    pele_params_archives = PeleParameterArchives(pele_arguments_namespace.contrl, pele_arguments_namespace.resfold,
+                                                 pele_arguments_namespace.report, pele_arguments_namespace.traject)
+    pele_params_sim_values = PeleParameterSimulationValues(pele_arguments_namespace.cpus, pele_arguments_namespace.steps
+                                                           , pele_arguments_namespace.pele_eq_steps,
+                                                           pele_arguments_namespace.min_overlap,
+                                                           pele_arguments_namespace.max_overlap,
+                                                           pele_arguments_namespace.temperature,
+                                                           pele_arguments_namespace.seed,
+                                                           pele_arguments_namespace.steering,
+                                                           pele_arguments_namespace.translation_high,
+                                                           pele_arguments_namespace.rotation_high,
+                                                           pele_arguments_namespace.translation_low,
+                                                           pele_arguments_namespace.rotation_low,
+                                                           pele_arguments_namespace.radius_box)
+    pele_parameters = PeleParameters(pele_params_paths, pele_params_archives, pele_params_sim_values)
+
+    return pele_parameters
+
+
+def create_cluster_parameters_object(clustering_arguments_namespace):
+    cluster_parameters = ClusterParameters(clustering_arguments_namespace.distcont,
+                                           clustering_arguments_namespace.cluster_threshold,
+                                           clustering_arguments_namespace.epsilon,
+                                           clustering_arguments_namespace.condition,
+                                           clustering_arguments_namespace.metricweights,
+                                           clustering_arguments_namespace.nclusters,
+                                           clustering_arguments_namespace.pdbout,
+                                           clustering_arguments_namespace.banned,
+                                           clustering_arguments_namespace.limit)
+
+    return cluster_parameters
+
+
+def create_plop_parameters_object(plop_arguments_namespace):
+    plop_parameters = PlopParameters(plop_arguments_namespace.plop_path,
+                                     plop_arguments_namespace.sch_python,
+                                     plop_arguments_namespace.rotamers)
+
+    return plop_parameters
+
+
+def create_frag_parameters_object(complex_pdb, fragment_pdb, core_atom, fragment_atom, h_core, h_frag,
+                                  frag_standard_arguments_namespace):
+    structural_files_parameters = FragStructuralFilesParameters(complex_pdb, fragment_pdb)
+    structural_configuration_parameters = FragStructuralConfigurationParameters(core_atom, fragment_atom,
+                                                                                h_core, h_frag,
+                                                                                frag_standard_arguments_namespace.c_chain,
+                                                                                frag_standard_arguments_namespace.f_chain,
+                                                                                frag_standard_arguments_namespace.threshold_clash)
+    identification_parameters = FragIdentificationParameters(ID)
+    configuration_parameters = FragConfigurationParameters(frag_standard_arguments_namespace.growing_steps,
+                                                           frag_standard_arguments_namespace.criteria,
+                                                           frag_standard_arguments_namespace.sampling_control)
+    protocol_parameters = FragProtocolParameters(frag_standard_arguments_namespace.only_prepare,
+                                                 frag_standard_arguments_namespace.only_grow,
+                                                 frag_standard_arguments_namespace.explorative)
+    running_modes_parameters = FragRunningModesParameters(frag_standard_arguments_namespace.restart,
+                                                          frag_standard_arguments_namespace.no_check)
+    frag_parameters = FragParameters(structural_files_parameters, structural_configuration_parameters,
+                                     identification_parameters, configuration_parameters,
+                                     protocol_parameters, running_modes_parameters)
+
+    return frag_parameters
+
+
 if __name__ == '__main__':
     # Create Frag object
     frag_object = Frag()
-    #HOT FIX!! Fix it properly
+    # HOT FIX!! Fix it properly
     original_dir = os.path.abspath(os.getcwd())
 
     # Get arguments
-    complex_pdb, serie_file, no_check, growing_steps, criteria, restart, c_chain, f_chain, threshold_clash, sampling_control, \
-    only_prepare, only_grow, plop_path, sch_python, rotamers, pele_dir, contrl, license, resfold, report, traject, \
-    cpus, steps, pele_eq_steps, min_overlap, max_overlap, temperature, seed, steering, translation_high, rotation_high, \
-    translation_low, rotation_low, radius_box, data, documents, distcont, cluster_threshold, epsilon, condition, metricweights, \
-    nclusters, pdbout, banned, limit, explorative, mae, rename = parse_arguments()
+    arguments_dict = parse_arguments()
+    required_arguments_namespace = arguments_dict['required named arguments']  # todo change this keys to constants
+    frag_standard_arguments_namespace = arguments_dict['Frag Standard Arguments']
+    plop_arguments_namespace = arguments_dict['Plop Related Arguments']
+    pele_arguments_namespace = arguments_dict['PELE Related Arguments']
+    clustering_arguments_namespace = arguments_dict['Clustering Related Arguments']
+    mae = arguments_dict['optional arguments'].mae
+    rename = arguments_dict['optional arguments'].rename
 
-    list_of_instructions = serie_handler.read_instructions_from_file(serie_file)
+    list_of_instructions = serie_handler.read_instructions_from_file(required_arguments_namespace.serie_file)
     print("READING INSTRUCTIONS... You will perform the growing of {} fragments. GOOD LUCK and ENJOY the "
           "trip :)".format(len(list_of_instructions)))
-    dict_traceback = correct_fragment_names.main(complex_pdb)
+    dict_traceback = correct_fragment_names.main(required_arguments_namespace.complex_pdb)
     for instruction in list_of_instructions:
         # We will iterate trough all individual instructions of file.
         # SUCCESSIVE GROWING
@@ -96,27 +175,23 @@ if __name__ == '__main__':
                 except Exception:
                     traceback.print_exc()
                 try:
-                    serie_handler.check_instructions(instruction[i], complex_pdb, c_chain, f_chain)
+                    serie_handler.check_instructions(instruction[i], required_arguments_namespace.complex_pdb,
+                                                     frag_standard_arguments_namespace.c_chain,
+                                                     frag_standard_arguments_namespace.f_chain)
                     # Create PELE parameters objects
-                    pele_params_paths = PeleParameterPaths(pele_dir, license, data, documents)
-                    pele_params_archives = PeleParameterArchives(contrl, resfold, report, traject)
-                    pele_params_sim_values = PeleParameterSimulationValues(cpus, steps, pele_eq_steps, min_overlap,
-                                                                           max_overlap, temperature, seed, steering, translation_high,
-                                                                           rotation_high, translation_low, rotation_low, radius_box)
-                    pele_parameters = PeleParameters(pele_params_paths, pele_params_archives, pele_params_sim_values)
+                    pele_parameters = create_pele_parameter_object(pele_arguments_namespace)
                     # Create Cluster parameters object
-                    cluster_parameters = ClusterParameters(distcont, cluster_threshold, epsilon, condition,
-                                                           metricweights, nclusters, pdbout, banned, limit)
+                    cluster_parameters = create_cluster_parameters_object(clustering_arguments_namespace)
                     # Create Plop parameters object
-                    plop_parameters = PlopParameters(plop_path, sch_python, rotamers)
+                    plop_parameters = create_plop_parameters_object(plop_arguments_namespace)
+                    # Create Frag parameters object
+                    frag_parameters = create_frag_parameters_object(complex_pdb, fragment_pdb, core_atom, fragment_atom,
+                                                                    h_core, h_frag, frag_standard_arguments_namespace)
+
                     print("PERFORMING SUCCESSIVE GROWING...")
                     print("HYDROGEN ATOMS IN INSTRUCTIONS:  {}    {}".format(h_core, h_frag))
                     atomname_map = frag_object.run_frag(pele_parameters, cluster_parameters, plop_parameters,
-                                                        complex_pdb, fragment_pdb,
-                                                        core_atom, fragment_atom, growing_steps, criteria,
-                                                        restart, ID, h_core, h_frag, c_chain,
-                                                        f_chain, mae, rename, threshold_clash, explorative,
-                                                        sampling_control, only_prepare, only_grow, no_check)
+                                                        frag_parameters, mae, rename)
                     atomname_mappig.append(atomname_map)
                 except Exception:
                     os.chdir(original_dir)
@@ -140,27 +215,20 @@ if __name__ == '__main__':
                 h_core = None
                 h_frag = None
             try:
-                # Create PELE objects
-                pele_params_paths = PeleParameterPaths(pele_dir, license, data, documents)
-                pele_params_archives = PeleParameterArchives(contrl, resfold, report, traject)
-                pele_params_sim_values = PeleParameterSimulationValues(cpus, steps, pele_eq_steps, min_overlap,
-                                                                       max_overlap, temperature, seed, steering,
-                                                                       translation_high,
-                                                                       rotation_high, translation_low, rotation_low,
-                                                                       radius_box)
-                pele_parameters = PeleParameters(pele_params_paths, pele_params_archives, pele_params_sim_values)
-                # Create Cluster objects
-                cluster_parameters = ClusterParameters(distcont, cluster_threshold, epsilon, condition,
-                                                       metricweights, nclusters, pdbout, banned, limit)
+                serie_handler.check_instructions(instruction[i], complex_pdb, c_chain, f_chain)
+                # Create PELE parameters objects
+                pele_parameters = create_pele_parameter_object(pele_arguments_namespace)
+                # Create Cluster parameters object
+                cluster_parameters = create_cluster_parameters_object(clustering_arguments_namespace)
                 # Create Plop parameters object
-                plop_parameters = PlopParameters(plop_path, sch_python, rotamers)
+                plop_parameters = create_plop_parameters_object(plop_arguments_namespace)
+                # Create Frag parameters object
+                frag_parameters = create_frag_parameters_object(complex_pdb, fragment_pdb, core_atom, fragment_atom,
+                                                                h_core, h_frag, frag_standard_arguments_namespace)
                 print("PERFORMING INDIVIDUAL GROWING...")
                 print("HYDROGEN ATOMS IN INSTRUCTIONS:  {}    {}".format(h_core, h_frag))
                 frag_object.run_frag(pele_parameters, cluster_parameters, plop_parameters,
-                                     complex_pdb, fragment_pdb, core_atom, fragment_atom, growing_steps, criteria,
-                                     restart, ID, h_core,
-                                     h_frag, c_chain, f_chain, mae, rename,
-                                     threshold_clash, explorative, sampling_control, only_prepare, only_grow, no_check)
+                                     frag_parameters)
             except Exception:
                 os.chdir(original_dir)
                 traceback.print_exc()
