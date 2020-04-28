9bfeec906db5503a6b771f7a52719f85d8d6434e
Alberto <Alberto.soutullo@hotmail.com>
2020-04-28 19:45:21 +0200

    moved explorative to frag protocol parameters

--- frag_pele/frag/FragParameters/frag_protocol_parameters.py
+++ frag_pele/frag/FragParameters/frag_protocol_parameters.py
@@ -7,14 +7,16 @@
 
 class FragProtocolParameters:
 
-    def __init__(self, only_prepare: bool, only_grow: bool):
+    def __init__(self, only_prepare: bool, only_grow: bool, explorative: bool):
         """
         :param only_prepare = todo description
         :param only_grow = todo description
+        :param explorative = todo description
         """
         # Protocol
         self._only_prepare = only_prepare
         self._only_grow = only_grow
+        self._explorative = explorative
 
     # Properties
     @property
@@ -24,3 +26,7 @@ class FragProtocolParameters:
     @property
     def only_grow(self):
         return self._only_grow
+
+    @property
+    def explorative(self):
+        return self._explorative
