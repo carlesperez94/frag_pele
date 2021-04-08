1cca595f04f8a2f93573334e54a4f039d051bc45
Carles Perez Lopez <carlesperez94@gmail.com>
2021-04-08 10:40:17 +0200

    Hot fix in template fragmenter because wrong length of line RESX

--- frag_pele/Growing/template_fragmenter.py
+++ frag_pele/Growing/template_fragmenter.py
@@ -6,7 +6,7 @@ logger = logging.getLogger(__name__)
 # Definition of patterns
 HEADER_OPLS2005 = "* LIGAND DATABASE FILE (frag_pele)\n*\n"
 PATTERN_OPLS2005_RESX_HEADER = "{:5} {:6d} {:6d} {:7d} {:7d} {:8d} \n"
-PATTERN_OPLS2005_RESX_LINE = "{:5d} {:5d} {:1}  {:4}  {:4} {:5d}{: >11.5f} {: >11.5f} {: >11.5f}\n"
+PATTERN_OPLS2005_RESX_LINE = "{:5d} {:5d} {:1}  {:4}  {:4} {:5d} {: >11.6f} {: >11.6f} {: >11.6f}\n"
 PATTERN_OPLS2005_NBON = " {:5d} {: >8.4f} {: >8.4f} {: >10.6f} {: >8.4f} {: >8.4f} {: >13.9f} {: >13.9f}\n"
 PATTERN_OPLS2005_BOND = " {:5d} {:5d} {:>9.3f} {:>6.3f}\n"
 PATTERN_OPLS2005_THETA = " {:5d} {:5d} {:5d} {:>11.5f}{: >11.5f}\n"
